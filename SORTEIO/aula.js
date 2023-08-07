905b376ed2e87285c216d72910e88bce52a19924
letsouzars <leticia.souza17senai@gmail.com>
2023-08-07 14:03:09 -0300

    ariba
    .

---
+++ SORTEIO/aula.js
@@ -0,0 +1,17 @@
+// Math.random gera um numero aleatorio
+console.log(Math.random());
+
+// Gerando um numero aleatorio de 0 a 20
+console.log(Math.random() * 20);
+
+// Math.round arredonda para o numero inteiro mais proximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Math.ceil arredonda para cima 
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
